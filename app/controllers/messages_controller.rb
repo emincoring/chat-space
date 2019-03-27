1502e7149b4f581c251103b244d124c829a50914
emincoring <emiri.a.812@gmail.com>
2019-03-27 18:59:00 +0900

    Add message controller and model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,5 +3,11 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :image, :group_id, :user_id)
   end
 end
