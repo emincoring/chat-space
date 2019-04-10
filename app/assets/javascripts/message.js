7ca7c339c62b1bb35e402ca470184cbe729c3622
emincoring <emiri.a.812@gmail.com>
2019-04-10 20:22:39 +0900

    Add automatic update function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message){
-    var imageHTML = (message.image) ? `<img class="message__image" src=${message.image.url}>` :  "";
-    var html = `<div class="message">
+    var image = message.image.url ? message.image.url : "";
+    var html = `<div class="message" data-id=${message.id}>
                   <div class="message__info">
                     <p class="message__info__user">
                       ${message.user_name}
@@ -14,11 +14,12 @@ $(function(){
                     <p class="message__text__content">
                       ${message.content}
                     </p>
-                    ${imageHTML}
+                    <img class="message__image" src=${image}>
                   </div>
                 </div>`;
     return html;
   }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -34,9 +35,6 @@ $(function(){
     .done(function(message){
       var html = buildHTML(message);
       $('.messages').append(html);
-      $('img').error(function(){
-        $(this).remove();
-      });
       $('#new_message')[0].reset();
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
@@ -47,4 +45,29 @@ $(function(){
       $('.submit-btn').removeAttr('disabled');
     })
   });
+
+  var reloadMessages = function(){
+    last_message_id = $(".message:last").data('id');
+    pathname = location.pathname.split('/')[2];
+    $.ajax({
+      url:      `/groups/${pathname}/api/messages`,
+      type:     'GET',
+      dataType: 'json',
+      data:     {id: last_message_id}
+    })
+    .done(function(messages){
+      var insertHTML = '';
+      if (messages.length !== 0) {
+        messages.map(function(message){
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+        });
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    };
+    })
+    .fail(function(){
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 5000);
 });
